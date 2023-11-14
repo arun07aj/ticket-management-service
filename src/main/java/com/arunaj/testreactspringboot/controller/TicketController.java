5d9efe8e32ac8a63b4c19f56f32f7a3e8c356cc5
Arun <arun07aj@gmail.com>
2023-11-14 15:26:47 +0530

    added controller and service for new ticket creation

---
+++ src/main/java/com/arunaj/testreactspringboot/controller/TicketController.java
@@ -0,0 +1,34 @@
+package com.arunaj.testreactspringboot.controller;
+
+import com.arunaj.testreactspringboot.model.Ticket;
+import com.arunaj.testreactspringboot.service.TicketService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tickets")
+public class TicketController {
+    private static final Logger logger = LoggerFactory.getLogger(TicketController.class);
+
+    @Autowired
+    private TicketService ticketService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createTicket(@RequestBody Ticket ticket) {
+        try {
+            Ticket newTicket = ticketService.createTicket(ticket);
+            return new ResponseEntity<>(newTicket, HttpStatus.CREATED);
+        }
+        catch(Exception e) {
+            logger.error("Error creating ticket:", e);
+            return new ResponseEntity<>("Error creating ticket", HttpStatus.BAD_REQUEST);
+        }
+    }
+}
---
+++ src/main/java/com/arunaj/testreactspringboot/service/TicketService.java
@@ -0,0 +1,34 @@
+package com.arunaj.testreactspringboot.service;
+
+import com.arunaj.testreactspringboot.model.Ticket;
+import com.arunaj.testreactspringboot.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TicketService {
+
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    public Ticket createTicket(Ticket ticket) throws Exception {
+
+        if(ticket != null) {
+            ticket.setStatus("OPEN");
+            ticket.setTime(new Date(System.currentTimeMillis()));
+
+            if(isValidTicket(ticket)) {
+                return ticketRepository.save(ticket);
+            }
+        }
+        throw new Exception("invalid ticket");
+    }
+
+    private boolean isValidTicket(Ticket ticket) {
+        // Check for non-null and non-empty values
+        return ticket.getDescription() != null && !ticket.getDescription().isEmpty() &&
+                ticket.getSubject() != null && !ticket.getSubject().isEmpty();
+    }
+}
