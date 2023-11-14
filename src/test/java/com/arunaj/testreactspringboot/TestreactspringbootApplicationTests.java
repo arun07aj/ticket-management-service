d4efda057a9a0e40a05e57570e30242f51febd2d
Arun <arun07aj@gmail.com>
2023-11-14 12:33:46 +0530

    initial springboot commit

---
+++ src/test/java/com/arunaj/testreactspringboot/TestreactspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arunaj.testreactspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestreactspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
