8d032d572cfe0d97425b713e699772c160943e77
Arun <arun07aj@gmail.com>
2023-11-15 23:09:55 +0530

    impl ticket list frontend and styling

--- webapp/src/components/TicketList.js
+++ webapp/src/components/TicketList.js
@@ -1,13 +1,50 @@
-// src/components/TicketList.js
-import React from 'react';
+// TicketList.js
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import './TicketList.css';
 
-function TicketList() {
+const TicketList = () => {
+    const [tickets, setTickets] = useState([]);
+
+    useEffect(() => {
+        // Fetch tickets from the /list API endpoint
+        axios.get('/tickets/list')
+            .then(response => setTickets(response.data))
+            .catch(error => console.error('Error fetching tickets:', error));
+    }, []);
+
     return (
-        <div>
-            <h2>View Tickets</h2>
-            {/* Your TicketList component goes here */}
+        <div className="ticket-list-container">
+            <h2 className="table-title">All Tickets Status</h2>
+            <table className="ticket-table">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Subject</th>
+                    <th>Description</th>
+                    <th>Date</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                {tickets.map((ticket) => (
+                    <tr key={ticket.id}>
+                        <td>{ticket.id}</td>
+                        <td>{ticket.subject}</td>
+                        <td>{ticket.description}</td>
+                        <td>{ticket.time}</td>
+                        <td>{ticket.status}</td>
+                        <td>
+                            <Link to={`/tickets/${ticket.id}`}>{`View Ticket #${ticket.id}`}</Link>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </div>
     );
-}
+};
 
 export default TicketList;
