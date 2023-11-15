b2fe004c305eae9e3c010e1d38c87332bf834001
Arun <arun07aj@gmail.com>
2023-11-15 23:45:58 +0530

    added frontend for viewing single ticket details

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import TicketForm from './components/TicketForm';
 import TicketList from './components/TicketList';
+import ViewTicket from "./components/ViewTicket";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
               <Route path="/" exact element={<Home />} />
               <Route path="/create-ticket" element={<TicketForm />} />
               <Route path="/view-tickets" element={<TicketList />} />
+              <Route path="/tickets/:id" element={<ViewTicket />} />
           </Routes>
       </Router>
   );
---
+++ webapp/src/components/ViewTicket.js
@@ -0,0 +1,40 @@
+// ViewTicket.js
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+const ViewTicket = () => {
+    const { id } = useParams();
+
+    const [ticketDetails, setTicketDetails] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        axios.get(`/tickets/list/${id}`)
+            .then(response => setTicketDetails(response.data))
+            .catch(error => {
+                console.error('Error fetching ticket details:', error);
+                setError(error);
+            });
+    }, [id]);
+
+    if (error) {
+        return <div>Error loading ticket details. Please try again later.</div>;
+    }
+
+    if (!ticketDetails) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <div>
+            <h1>{`View Ticket #${ticketDetails.id}`}</h1>
+            <p><strong>Subject:</strong> {ticketDetails.subject}</p>
+            <p><strong>Description:</strong> {ticketDetails.description}</p>
+            <p><strong>Created Time:</strong> {ticketDetails.time}</p>
+            <p><strong>Status:</strong> {ticketDetails.status}</p>
+        </div>
+    );
+};
+
+export default ViewTicket;
