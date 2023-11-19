7bc37f7ab197075996fb2c4c0e5e7f37e1fc3092
Arun <arun07aj@gmail.com>
2023-11-19 19:46:35 +0530

    changed TicketList component to only show tickets owned by current user

--- webapp/src/components/TicketList.js
+++ webapp/src/components/TicketList.js
@@ -2,21 +2,24 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import Cookies from 'js-cookie';
 import './TicketList.css';
 
 const TicketList = () => {
     const [tickets, setTickets] = useState([]);
+    // Retrieve the JWT token from the cookie
+    const jwtToken = Cookies.get('jwtToken');
 
     useEffect(() => {
         // Fetch tickets from the /list API endpoint
-        axios.get('/tickets/list')
+        axios.get('/tickets/list/my', {headers: {Authorization: `Bearer ${jwtToken}`}})
             .then(response => setTickets(response.data))
             .catch(error => console.error('Error fetching tickets:', error));
     }, []);
 
     return (
         <div className="ticket-list-container">
-            <h2 className="table-title">All Tickets Status</h2>
+            <h2 className="table-title">My Tickets</h2>
             <table className="ticket-table">
                 <thead>
                 <tr>
